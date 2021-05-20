5bd6002f049f8bd04b3a276628fbd7c02d05d1d6
Alejandro Vasquez <alejandrovasquez@Alejandros-iMac.local>
2021-05-20 14:28:03 -0700

    Day 2

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+
 
 
 function NasaPhoto() {
@@ -15,16 +17,64 @@ function NasaPhoto() {
     })
 
 }, [], )
+
+const Container = styled.div`
+    display: flex
+    flex-direction: column;
+    justify-content: center;
+    vertical-align: bottom;
+    text-align: center;
+    background-color: grey;
+    min-height: 100vh;
+    margin-left: auto;
+    margin-right: auto:
+    padding: 25%
+`
+
+const Paragraph = styled.p`
+    color: blue;
+    padding 15px;
+    border-style: solid;
+    border-color: red;
+    margin-left: auto;
+    margin-right: auto;
+    width: 48%;
+
+`
+
+const Photo = styled.img`
+
+    margin-left: auto;
+    margin-right: auto:
+    max-height: 90vh;
+`
+
+
+    
+
 return (
-    <div>
-        <img src={photoData.url} alt= '' />
-        <div>
+    <Container>
+        {photoData.media_type === "image" ? (
+        <Photo src={photoData.url} alt={photoData.title}/>
+        ) : (
+            <iframe
+                title="space-video"
+                src={photoData.url}
+                frameBorder="0"
+                gesture="media"
+                allow="encrypted-media"
+                allowFullScreen
+                className="photo"
+            />
+        )}
+        
+        <Paragraph>
             <h1>{photoData.title}</h1>
             <p>{photoData.date}</p>
             <p>{photoData.explanation}</p>
-        </div>
+        </Paragraph>
 
-    </div>
+    </Container>
 );
 }
 export default NasaPhoto;
